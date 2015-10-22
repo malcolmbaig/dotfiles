29218e6f9164da2620fb9f0c7efa97b3d5a4a6bd
Mo Baig <mo@mobaig.com>
2015-10-22 10:40:39 +0100

    Add tmux to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-DIR='~/Dropbox/dev_env/dotfiles'
+DIR='/Users/mo/Dropbox/dev_env/dotfiles'
 
 # NVIM
 ln -sf "$DIR/nvim/nvimrc" ~/.nvimrc
+ln -sf "$DIR/nvim" ~/.nvim
 
 # SSH
 ln -sf "$DIR/ssh/config" ~/.ssh/config
@@ -13,7 +14,7 @@ ln -sf "$DIR/ssh/config" ~/.ssh/config
 ln -sf "$DIR/git/gitignore-global" ~/.gitignore-global
 ln -sf "$DIR/git/git-template" ~/.git-template
 ln -sf "$DIR/git/gitmessage" ~/.gitmessage
-ln -sf ~/Dropbox/dev_env/dotfiles/git/gitconfig ~/.gitconfig
+ln -sf "$DIR/git/gitconfig" ~/.gitconfig
 
 # ZSH
 ln -sf "$DIR/zprezto/" ~/.zprezto
@@ -23,3 +24,8 @@ ln -sf ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
 ln -sf ~/.zprezto/runcoms/zprofile ~/.zprofile
 ln -sf ~/.zprezto/runcoms/zshenv ~/.zshenv
 ln -sf ~/.zprezto/runcoms/zlogin ~/.zlogin
+
+# Tmux
+ln -sf "$DIR/tmux" ~/.tmux
+ln -sf "$DIR/tmux/tmux.conf" ~/.tmux.conf
+ln -sf "$DIR/tmux/tmuxline_snapshot" ~/.tmuxline_snapshot
