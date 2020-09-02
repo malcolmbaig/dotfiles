518c1d71101796ae485f2e67f82194e16fd57196
Malcolm Baig <malcolm.baig@gmail.com>
2020-09-02 14:59:55 +0100

    Add basic bootstrap script for Manjaro

---
+++ bootstrap-manjaro.sh
@@ -0,0 +1,19 @@
+sudo pacman -S base-devel
+cd ~
+mkdir code
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay dropbox
+yay stow
+yay fzf
+yay tig
+yay tmux
+yay neovim
+yay rbenv
+yay git-delta
+yay iosevka-fixed
+
+cd ~/.dotfiles
+stow aliases ctags fzf.linux git nvim tig tmux zsh konsole
