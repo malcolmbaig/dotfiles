b6065c6bdaf047e67a053580e7fb43a1de6302a5
Malcolm Baig <git@baig.org>
2023-08-14 10:38:08 +0100

    nvim - remove lualine tabline config

--- nvim/.config/nvim/lua/lualine-custom.lua
+++ nvim/.config/nvim/lua/lualine-custom.lua
@@ -19,27 +19,5 @@ require 'lualine' .setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {
-    lualine_a = {
-      {
-        'tabs',
-        -- 0: Shows tab_nr
-        -- 1: Shows tab_name
-        -- 2: Shows tab_nr + tab_name
-        mode = 1,
-        max_length = 1000,
-        tabs_color = {
-          -- Same values as the general color option can be used here.
-          active = 'lualine_a_normal',     -- Color for active tab.
-          inactive = 'lualine_a_inactive', -- Color for inactive tab.
-        },
-      }
-    },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
   extensions = {}
 }
