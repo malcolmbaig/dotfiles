f41644ed510040e51804a72d62f9efb30995e7eb
Malcolm Baig <git@baig.org>
2023-08-17 15:01:30 +0100

    nvim - add lualine tab config with modified status

--- nvim/.config/nvim/lua/lualine-custom.lua
+++ nvim/.config/nvim/lua/lualine-custom.lua
@@ -19,5 +19,29 @@ require 'lualine' .setup {
     lualine_y = {},
     lualine_z = {}
   },
-  extensions = {}
+  tabline = {
+    lualine_a = {
+      {
+        'tabs',
+        mode = 1 ,
+        fmt = function(name, context)
+          -- Show + if buffer is modified in tab
+          local buflist = vim.fn.tabpagebuflist(context.tabnr)
+          local winnr = vim.fn.tabpagewinnr(context.tabnr)
+          local bufnr = buflist[winnr]
+          local mod = vim.fn.getbufvar(bufnr, '&mod')
+
+          return name .. (mod == 1 and ' [+]' or '')
+        end
+      },
+    },
+    lualine_b = {
+
+    },
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  extensions = {},
 }
