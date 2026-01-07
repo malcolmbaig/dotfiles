bce7eb4bdc7f9ddd81e4383174a687a5817aa71f
Malcolm Baig <git@baig.org>
2026-01-07 17:18:16 +0000

    nvim - add lualine and tabline formatting

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,30 @@
+-- Lualine statusline for a clean, informative bar
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      theme = "auto",
+      icons_enabled = true,
+      globalstatus = true,
+      section_separators = "",
+      component_separators = "",
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch" },
+      lualine_c = { { "filename", path = 1 } },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { { "filename", path = 1 } },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+  },
+}
