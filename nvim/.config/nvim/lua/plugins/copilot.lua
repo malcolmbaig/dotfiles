bb26837dcfc88ea099c7a4394378b3824c709fab
Malcolm Baig <git@baig.org>
2026-01-08 15:24:48 +0000

    nvim - add copilot

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+-- GitHub Copilot (copilot.lua)
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+    },
+  },
+}
