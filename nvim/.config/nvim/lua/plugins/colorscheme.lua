6d6059fc3135dac872ecd640aae65d461ed181de
Malcolm Baig <git@baig.org>
2026-01-09 23:14:29 +0000

    nvim - fix evergarden colorscheme config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -29,24 +29,26 @@ return {
   name = 'evergarden',
   lazy = false,
   priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-  opts = {
-    theme = {
-      variant = 'winter', -- 'winter'|'fall'|'spring'|'summer'
-      accent = 'green',
-    },
-    editor = {
-      transparent_background = false,
-      sign = { color = 'none' },
-      float = {
-        color = 'mantle',
-        solid_border = false,
-      },
-      completion = {
-        color = 'surface0',
-      },
-    },
-  },
   config = function()
+    require("evergarden").setup(
+      {
+          theme = {
+            variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
+            accent = 'green',
+          },
+          editor = {
+            transparent_background = false,
+            sign = { color = 'none' },
+            float = {
+              color = 'mantle',
+              solid_border = false,
+            },
+            completion = {
+              color = 'surface0',
+            },
+          },
+      }
+    )
     vim.cmd("colorscheme evergarden")
   end,
 }
