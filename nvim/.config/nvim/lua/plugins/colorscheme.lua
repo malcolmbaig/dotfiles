82a9f64edb524b9b7959cad04e5a6bd2dc1ec972
Malcolm Baig <git@baig.org>
2026-01-09 21:14:27 +0000

    nvim - switch colorscheme to evergarden

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,52 @@
--- Gruvbox Material colorscheme (dark hard)
+-- return {
+--   "sainnhe/gruvbox-material",
+--   lazy = false,
+--   priority = 1000,
+--   init = function()
+--     vim.o.background = "dark"
+--     vim.g.gruvbox_material_background = "medium"
+--   end,
+--   config = function()
+--     vim.cmd("colorscheme gruvbox-material")
+--   end,
+-- }
+
+-- return {
+--   "vague-theme/vague.nvim",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other plugins
+--   config = function()
+--     -- NOTE: you do not need to call setup if you don't want to.
+--     require("vague").setup({
+--       -- optional configuration here
+--     })
+--     vim.cmd("colorscheme vague")
+--   end
+-- }
+
 return {
-  "sainnhe/gruvbox-material",
+  'everviolet/nvim',
+  name = 'evergarden',
   lazy = false,
-  priority = 1000,
-  init = function()
-    vim.o.background = "dark"
-    vim.g.gruvbox_material_background = "hard"
-  end,
+  priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+  opts = {
+    theme = {
+      variant = 'winter', -- 'winter'|'fall'|'spring'|'summer'
+      accent = 'green',
+    },
+    editor = {
+      transparent_background = false,
+      sign = { color = 'none' },
+      float = {
+        color = 'mantle',
+        solid_border = false,
+      },
+      completion = {
+        color = 'surface0',
+      },
+    },
+  },
   config = function()
-    vim.cmd("colorscheme gruvbox-material")
+    vim.cmd("colorscheme evergarden")
   end,
 }
