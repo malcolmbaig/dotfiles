7ed5820c3416fc63f55e99af2d8bb32e72b86c57
Malcolm Baig <git@baig.org>
2026-01-09 21:19:49 +0000

    nvim - make nvim-tree use a floating window

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,4 +1,7 @@
 -- File explorer with icons
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5  -- You can change this too
+
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = {
@@ -16,8 +19,35 @@ return {
     sort = {
       sorter = "case_sensitive",
     },
+    respect_buf_cwd = true,
+    sync_root_with_cwd = true,
     view = {
-      width = 50,
+      relativenumber = true,
+      float = {
+        enable = true,
+        open_win_config = function()
+          local screen_w = vim.opt.columns:get()
+          local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+          local window_w = screen_w * WIDTH_RATIO
+          local window_h = screen_h * HEIGHT_RATIO
+          local window_w_int = math.floor(window_w)
+          local window_h_int = math.floor(window_h)
+          local center_x = (screen_w - window_w) / 2
+          local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                           - vim.opt.cmdheight:get()
+          return {
+            border = "rounded",
+            relative = "editor",
+            row = center_y,
+            col = center_x,
+            width = window_w_int,
+            height = window_h_int,
+          }
+          end,
+      },
+      width = function()
+        return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+      end,
     },
     renderer = {
       group_empty = true,
@@ -32,7 +62,6 @@ return {
     },
     filters = {
       dotfiles = false,
-      custom = { "^.git$" },
     },
     git = {
       enable = true,
